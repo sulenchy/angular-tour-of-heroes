5b784010ec96a9037928f0b4d3f64ab838c302fd
sulenchy <sulaimanabiodun172@gmail.com>
2019-07-18 20:27:20 +0100

    integrate routing and httpclient`

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService }  from '../hero.service';
 import { Hero } from '../hero';
 
 @ Component({
@@ -10,8 +14,28 @@ export class HeroDetailComponent implements OnInit {
 
   @ Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this .getHero();
+  }
+
+  getHero(): void {
+    const id = +this .route.snapshot.paramMap.get('id');
+    this .heroService .getHero(id)
+      .subscribe(hero => this .hero = hero);
+  }
+
+  goBack(): void {
+    this .location.back();
+  }
+
+  save(): void {
+    this .heroService.updateHero(this .hero)
+      .subscribe(() => this .goBack());
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,30 +12,30 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
-
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this .getHeroes();
   }
 
-  // getHeroes(): void {
-  //   this .heroes = this .heroService.getHeroes();
-  // }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this .heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this .heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this .heroes = this .heroes.filter(h => h !== hero);
+    this .heroService.deleteHero(hero).subscribe();
+  }
 
   getHeroes(): void {
     this .heroService.getHeroes()
         .subscribe(heroes => this .heroes = heroes);
   }
 
-  onSelect(hero: Hero): void {
-    this .selectedHero = hero;
-  }
 
 }
